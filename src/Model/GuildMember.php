6fe865a0d21b3d6a22d9d8448b49ebab1afb33f3
Quentin Bonaventure <quentin@bonaventure.xyz>
2018-06-21 15:18:29 +0200

    WIP

--- src/Model/GuildMember.php
+++ src/Model/GuildMember.php
@@ -8,10 +8,16 @@ class GuildMember
     
     private $id;
     
-    private function __construct(int $id, string $username)
+    /**
+     * @var GuildRole[]
+     */
+    private $roles;
+    
+    private function __construct(int $id, string $username, array $roles = null)
     {
         $this->id = $id;
         $this->username = $username;
+        $this->roles = $roles;
     }
     
     public function getUsername()
@@ -24,10 +30,30 @@ class GuildMember
         return $this->id;
     }
     
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+    
     public static function register(int $id, string $username) : GuildMember
     {  
         $member = new GuildMember($id, $username);
         return $member;
+    }
+    
+    public function toArray() : array
+    {
+        $roles = array_map(function($obj) { return $obj->toArray(); }, $this->roles);
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'roles' => $roles,
+        ];
+    }
+    
+    public static function fromDb(array $data) : GuildMember
+    {
+        return new GuildMember($data['id'], $data['username'], $data['roles']);
     }
     
 }
